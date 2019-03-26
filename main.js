5bf160617ab2e82949cc74e172a490cd58d6c86b
mateuszseczkowski <mateuszseczkowski@gmail.com>
2019-03-26 14:01:19 +0100

    currency added to object flat, average price calc

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 class Flat {
-    constructor(name, localization, price) {
+    constructor(name, localization, price, currency) {
         this.name = name;
         this.localization = localization;
         this.price = price;
+        this.currency = currency;
     }
 }
 
@@ -23,18 +24,25 @@ const fs = require('fs');
           return console.log(err);
       }
     }); 
-    console.log($('span.offer-item-title', html).length);
+    console.log("Number of objects: "+$('span.offer-item-title', html).length);
     for (let i = 0; i < $('span.offer-item-title', html).length; i++) {
       flats.push(
         new Flat(
           $('span.offer-item-title', html).map(function() {return $(this).text();}).toArray()[i], //name
           $('p.text-nowrap.hidden-xs', html).map(function() {return $(this).text().split(": ");}).toArray()[i], //localization
-          $('li.offer-item-price', html).map(function() {return $(this).text().trim();}).toArray()[i] //price
+          $('li.offer-item-price', html).map(function() {return $(this).text().trim().replace(/[^\d.,]/g, '');}).toArray()[i], //price (with float with . and ,)
+          $('li.offer-item-price', html).map(function() {return $(this).text().trim().replace(/[^\D]/g, '').trim();}).toArray()[i] //currency
         ) 
       );
     }
-    
-    console.log(flats[1]);
+    var total = 0;
+    for(var i = 0; i < flats.length; i++) {
+        total += parseInt(flats[i].price);
+    }
+    var avg = total / flats.length;
+
+    console.log(flats);
+    console.log("Average price: " + avg);
     // return Promise.all(
     //   flats.map(function(url) {
     //     return parseFunction('https://en.wikipedia.org' + url);
