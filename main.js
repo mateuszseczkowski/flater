5c9ffccb4b05519f51b23e60c751af7fd161e81f
MateuszSeczkowski <mateuszseczkowski@gmail.com>
2023-07-06 14:16:42 +0200

    cleanup

--- main.js
+++ main.js
@@ -1,72 +1,80 @@
-#!/usr/bin/env node
+import $ from "cheerio";
+import dateFormat from "dateformat";
+import fetch from "node-fetch";
+
+import { wstreamArray, wstreamAverage } from "./streamToFile";
+
+let date = dateFormat(new Date(), "dd.mm.yyyy hh:MM");
+const url =
+  "https://www.otodom.pl/sprzedaz/mieszkanie/lodz/?search%5Bdist%5D=0&search%5Bcity_id%5D=1004&nrAdsPerPage=72";
 
 class Flat {
-    constructor(name, localization, price, currency) {
-        this.name = name;
-        this.localization = localization;
-        this.price = price;
-        this.currency = currency;
-    }
+  constructor(name, localization, price, currency) {
+    this.name = name;
+    this.localization = localization;
+    this.price = price;
+    this.currency = currency;
+  }
 }
 
-const rp = require('request-promise');
-const $ = require('cheerio');
-const parseFunction = require('./parseFunction');
-const streamToFile = require('./streamToFile');
-const url = 'https://www.otodom.pl/sprzedaz/mieszkanie/lodz/?search%5Bdist%5D=0&search%5Bcity_id%5D=1004&nrAdsPerPage=72';
-const fs = require('fs');
-var dateFormat = require('dateformat');
-var date = dateFormat(new Date(), "dd.mm.yyyy hh:MM");
-
-  rp(url)
-  .then(function(html) {
-    //success!
+fetch(url)
+  .then(function (html) {
     const flats = [];
-    // fs.writeFile("pageBody.htm", html, function(err) {
-    //   if(err) {
-    //       return console.log(err);
-    //   }
-    // });
-    console.log("Number of objects: "+$('span.offer-item-title', html).length);
-    for (let i = 0; i < $('span.offer-item-title', html).length; i++) {
+    console.log(
+      "Number of objects: " + $("span.offer-item-title", html).length
+    );
+
+    for (let i = 0; i < $("span.offer-item-title", html).length; i++) {
       flats.push(
         new Flat(
-          $('span.offer-item-title', html).map(function() {return $(this).text();}).toArray()[i], //name
-          $('p.text-nowrap.hidden-xs', html).map(function() {return $(this).text().split(": ");}).toArray()[i], //localization
-          $('li.offer-item-price', html).map(function() {return $(this).text().trim().replace(/[^\d.,]/g, '');}).toArray()[i], //price (with float with . and ,)
-          $('li.offer-item-price', html).map(function() {return $(this).text().trim().replace(/[^\D]/g, '').trim();}).toArray()[i] //currency
+          $("span.offer-item-title", html)
+            .map(function () {
+              return $(this).text();
+            })
+            .toArray()[i], //name
+          $("p.text-nowrap.hidden-xs", html)
+            .map(function () {
+              return $(this).text().split(": ");
+            })
+            .toArray()[i], //localization
+          $("li.offer-item-price", html)
+            .map(function () {
+              return $(this)
+                .text()
+                .trim()
+                .replace(/[^\d.,]/g, "");
+            })
+            .toArray()[i], //price (with float with . and ,)
+          $("li.offer-item-price", html)
+            .map(function () {
+              return $(this).text().trim().replace(/[^\D]/g, "").trim();
+            })
+            .toArray()[i] //currency
         )
       );
     }
 
-    for (var i = flats.length - 1; i >= 0; i--) {
-      if (parseInt(flats[i].price) < 60000) { //filter to low prices (prywatne kamienice)
+    for (let i = flats.length - 1; i >= 0; i--) {
+      if (parseInt(flats[i].price) < 60000) {
+        //filter too low prices (prywatne kamienice)
         flats.splice(i, 1);
       }
     }
 
     console.log("Number of objects after filtering: " + flats.length);
 
-    var total = 0;
-    for(var i = 0; i < flats.length; i++) {
-        total += parseInt(flats[i].price);
+    let total = 0;
+    for (let i = 0; i < flats.length; i++) {
+      total += parseInt(flats[i].price);
     }
-    var averagePriceOfAllFlats = total / flats.length;
+    let averagePriceOfAllFlats = total / flats.length;
 
-    streamToFile.wstreamArray.write(JSON.stringify(flats));
-    streamToFile.wstreamArray.end();
+    wstreamArray.write(JSON.stringify(flats));
+    wstreamArray.end();
 
-    streamToFile.wstreamAverage.write(date + ";" + averagePriceOfAllFlats + "\n");
-    streamToFile.wstreamAverage.end();
-    // return Promise.all(
-    //   flats.map(function(url) {
-    //     return parseFunction('https://en.wikipedia.org' + url);
-    //   })
-    // );
+    wstreamAverage.write(date + ";" + averagePriceOfAllFlats + "\n");
+    wstreamAverage.end();
   })
-  // .then(function(presidents) {
-  //   console.log(presidents);
-  // })
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(err);
   });
